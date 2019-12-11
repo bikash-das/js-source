5ac44b2e5982fe01c6473130ce5a61e3f53bc4af
Bikash Das <dasbikash989@gmail.com>
2019-12-11 22:34:53 +0530

    creation of object with properties and methods

---
+++ object_literal.js
@@ -0,0 +1,16 @@
+const circle = {
+	//key value pairs collection
+	radius: 1,
+	location: {
+		//can have object
+		x:1,
+		y:1
+	},
+	draw: function() {
+		console.log('draw');
+	}
+};
+
+//acess member using dot notation
+
+circle.draw();
